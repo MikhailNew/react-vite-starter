4d1ac82d97884db8c518d0866c983a7275cb15e1
Michael <asdbul130391@gmail.com>
2022-01-13 22:16:01 +0100

    added configs, prettier, eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          gzipSize: true,
+          brotliSize: true,
+        }) as unknown as Plugin,
+      ],
+    },
+  },
 })
